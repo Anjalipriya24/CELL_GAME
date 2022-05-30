b337c70a91edf06fad3524feb1818f2e1ed805af
Somya Mahapatra <ritul.mahapatra@gmail.com>
2022-05-30 22:35:33 +0530

    Update entrypoint.sh

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -17,6 +17,7 @@ shift $(($OPTIND - 1))
 
 #version=$1
 echo "cd to github workspace"
+git config --global --add safe.directory /github/workspace
 cd ${GITHUB_WORKSPACE}
 git for-each-ref refs/tags/ --count=1 --sort=-version:refname --format='%(refname:short)'
 
